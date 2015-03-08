4c97447ed943cad402f17d407a71301d5e3616d2
Domrachev Denis <mentallmail@gmail.com>
2015-03-08 22:59:08 +0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,8 @@
+echo "Switch directory to RemoteActorCreationWithWebUI"
 cd /home/debian/RemoteActorCreationWithWebUI
+echo "Updating files from repository"
 git pull
+echo "Switch directory to RemoteApp"
 cd RemoteApp
+echo "Starting programm"
 sbt run
