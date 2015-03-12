15f564ce5068b2f81011be1bb70c36ba7b6773fd
Domrachev Denis <mentallmail@gmail.com>
2015-03-12 21:38:54 +0300

    Update Messages.scala

--- RemoteApp/src/main/scala/Messages.scala
+++ RemoteApp/src/main/scala/Messages.scala
@@ -11,12 +11,20 @@ case class CreateNewActor(var actorType: String, var actorId : String, var tcp :
 case class ActorCreated(val adr: ActorRef) extends Serializable{
   override def toString = "ActorRef:"+adr
 }
+@SerialVersionUID(21L)
 case object NonexistentActorType extends Serializable
+@SerialVersionUID(22L)
 case object CheckAddress extends Serializable
+@SerialVersionUID(23L)
 case object AddressIsOk extends Serializable
+@SerialVersionUID(24L)
 case object StopSystem extends Serializable
+@SerialVersionUID(25L)
 case object ConnectionRequest extends Serializable
+@SerialVersionUID(26L)
 case object Connected extends Serializable
 
+@SerialVersionUID(27L)
 case object TellYourIP extends Serializable
+@SerialVersionUID(28L)
 case class MyIPIs (val IP : String) extends Serializable
