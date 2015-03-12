4b9e05093ba03117f918743e4b9c35327b5c09e5
KNV <bakaknv@gmail.com>
2015-03-12 12:12:23 +0300

    RouterProvider commented and refactored

--- LocalApp/src/main/scala/RouterActorsProvider.scala
+++ LocalApp/src/main/scala/RouterActorsProvider.scala
@@ -11,7 +11,9 @@ import akka.pattern.ask
  */
 class RouterActorsProvider extends Actor {
   val logger : LoggingAdapter = Logging.getLogger(context.system, this)
+  // таблица с роутерами
   var remoteRouters = new mutable.HashMap[Long, ActorRef]
+  // загрузка роутера (число обслуживаемых юзеров, роутер)
   var routersLoad = new mutable.MutableList[(Long, Long)]
   var uniqueId = 0
 
@@ -20,39 +22,67 @@ class RouterActorsProvider extends Actor {
 
   implicit val timeout: Timeout = 1 minute
 
+  /**
+   * роутер подключается как ремот система в данной функции
+   * заносим его в список известных remoteRouters и говорит о том что на нём
+   * ещё никого нет ((0, uniqueId)) ((загрузка, роутер))
+   * @param sender
+   */
+
+  def onRouterConnectionRequest(sender: ActorRef) = {
+    logger.debug("Connection request")
+    uniqueId += 1
+    remoteRouters += ((uniqueId, sender))
+    routersLoad += ((0, uniqueId))
+    routersLoad = routersLoad.sorted
+    logger.debug("Sorted List : " + routersLoad.toString)
+    sender ! Connected
+  }
+
+  /**
+   * Http сервер при запросе регистрации от Http клиента берёт id клиента, генерит id автора
+   * запрашивает у роутера данный метод, стартует актора с коннетк стрингом и созданнымм
+   * id. Как-то так это будет происходить.
+   * @param sender
+   */
+
+  def onRegisterPairRequest(sender : ActorRef) = {
+    logger.debug("Registering pair : " + pair.clientID + " " + pair.actorID)
+    if (routersLoad.size > 0) {
+      logger.debug("Routers Load before register: " + routersLoad.toString)
+      //get router with minimum users
+      val routerId = routersLoad.head._2
+      //get router ref
+      val router = remoteRouters(routerId)
+      //adding user for router
+      routersLoad.head._1 += 1
+      //sorting list
+      routersLoad = routersLoad.sorted
+      logger.debug("Routers Load after register: " + routersLoad.toString)
+      //register new id's on router
+      val respForClient = Await.result((router ? SetMessage(pair.actorID)), 1 minute)
+      val respForActor = Await.result((router ? SetMessage(pair.clientID)), 1 minute)
+      val clientStr = respForClient.asInstanceOf[String]
+      val actorStr = respForActor.asInstanceOf[String]
+      logger.debug("Router response : (client: " + clientStr + " " + actorStr + ")")
+      //возвращаем зарегистрированные адреса тому кто попросил регистрацию
+      sender ! PairRegistered(clientStr, actorStr)
+    } else {
+      //если нет роутеров, то ничего не остаётся как послать клиента.
+      logger.debug("No Routers connected")
+      sender ! NoRouters
+    }
+  }
+
   override def receive : Receive = {
     case ConnectionRequest                =>  {
-      logger.debug("Connection request")
-      uniqueId += 1
-      remoteRouters += ((uniqueId, sender))
-      routersLoad += ((0, uniqueId))
-      routersLoad = routersLoad.sorted
-      logger.debug("Sorted List : " + routersLoad.toString)
-      sender ! Connected
+      onRouterConnectionRequest(sender)
     }
-
     /**
      * need to test this method
       */
     case pair: RegisterPair  => {
-      logger.debug("Registering pair : " + pair.clientID + " " + pair.actorID)
-      if (routersLoad.size > 0) {
-        logger.debug("Routers Load before register: " + routersLoad.toString)
-        val routerId = routersLoad.head._2
-        val router = remoteRouters(routerId)
-        routersLoad.head._1 += 1
-        routersLoad = routersLoad.sorted
-        logger.debug("Routers Load after register: " + routersLoad.toString)
-        val respForClient = Await.result((router ? SetMessage(pair.actorID)), 1 minute)
-        val respForActor = Await.result((router ? SetMessage(pair.clientID)), 1 minute)
-        val clientStr = respForClient.asInstanceOf[String]
-        val actorStr = respForActor.asInstanceOf[String]
-        logger.debug("Router response : (client: " + clientStr + " " + actorStr + ")")
-        sender ! PairRegistered(clientStr, actorStr)
-      } else {
-        logger.debug("No Routers connected")
-        sender ! NoRouters
-      }
+      onRegisterPairRequest(sender)
     }
     case msg => logger.debug("Unknown Message: " + msg.toString)
   }
