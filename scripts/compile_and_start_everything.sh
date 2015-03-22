ff6b0a7bff147c595aed90cfca1040208560e54c
mtl14 <mentallmail@gmail.com>
2015-03-22 14:49:09 +0300

    terminals tiling

--- scripts/compile_and_start_everything.sh
+++ scripts/compile_and_start_everything.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#1280*720
+
+let "addit_monitors_amount="$(xrandr -q | grep '\*' | wc -l)" - 1"
+echo "Additional monitors amount: $addit_monitors_amount"
+let "hor0 = addit_monitors_amount * 1366 + 360"
+
 echo "Compiling everything"
 cd LocalApp/
 sbt compile &
@@ -12,8 +18,10 @@ while [[ $(ps -ax | grep "sbt compile" | wc -l) != 1 ]]; do
 done
 echo "Starting everything"
 cd ../LocalApp/
-xfce4-terminal -H -e "sbt run"
+xfce4-terminal -H -e "sbt run" --geometry=75x18+0+hor0
+sleep 3
 cd ../RemoteApp/
-xfce4-terminal -H -e "sbt run"
+xfce4-terminal -H -e "sbt run" --geometry=75x18+0+hor0
+sleep 3
 cd ../MessageRouter/
-xfce4-terminal -H -e "sbt run"
+xfce4-terminal -H -e "sbt run" --geometry=75x18+0+hor0
