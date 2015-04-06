94a30ae452406c3443ff4c132f14dc4c5dad1ec5
KNV <bakaknv@gmail.com>
2015-04-06 14:15:12 +0300

    Рефакторинг. Денис, смотри комменты в app.py

--- operator-app/app.py
+++ operator-app/app.py
@@ -10,55 +10,53 @@ from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QGridLayout,
 import json
 import requests
 
+# Денис, есть такая штука, как PEP-8. Позязя, позязя, позязя, юзай его, ну позязя...
+# Иначе я тебя убью! Нагло и расчленённо!!! *devil_dance*
+# А то у меня IDE весь код подчёркмвает со словами "ты ебанько???!"
 
 class Example(QWidget):
-    
     def __init__(self):
         super().__init__()
+        self.ip_edit    = QLineEdit('http://127.0.0.1:8080')
+        self.uuid_edit  = QLineEdit()
+        self.init_ui()
         
-        self.initUI()        
-        
-    def initUI(self):        
+    def init_ui(self):        
         self.setGeometry(300, 300, 300, 220)
         self.setWindowTitle('Operator app')
         self.center()    
 
-        mainLayout    = QGridLayout(self)
-
-        self.ipEdit    = QLineEdit('http://127.0.0.1:8080')
-        ipTitle   = QLabel('IP:')
-        mainLayout.addWidget(ipTitle      , 0, 0)
-        mainLayout.addWidget(self.ipEdit  , 0, 1, 1, 3)
-        self.uuidEdit  = QLineEdit()
-        uuidTitle = QLabel('UUID:')
-        mainLayout.addWidget(uuidTitle    , 1, 0)
-        mainLayout.addWidget(self.uuidEdit, 1, 1, 1, 3)
-
-        frwdBtn   = QPushButton('Forward', self)
-        leftBtn   = QPushButton('Left', self)
-        bkwdBtn   = QPushButton('Backward', self)
-        rightBtn  = QPushButton('Right', self)
-        mainLayout.addWidget(frwdBtn, 3, 2)
-        mainLayout.addWidget(leftBtn, 4, 1)
-        mainLayout.addWidget(bkwdBtn, 4, 2)
-        mainLayout.addWidget(rightBtn, 4, 3)
-        frwdBtn.clicked.connect(self.buttonClicked)
-        leftBtn.clicked.connect(self.buttonClicked)
-        bkwdBtn.clicked.connect(self.buttonClicked)
-        rightBtn.clicked.connect(self.buttonClicked)
-
+        main_layout    = QGridLayout(self)
+        ip_title   = QLabel('IP:')
+        main_layout.addWidget(ip_title      , 0, 0)
+        main_layout.addWidget(self.ip_edit  , 0, 1, 1, 3)
+        uuid_title = QLabel('UUID:')
+        main_layout.addWidget(uuid_title    , 1, 0)
+        main_layout.addWidget(self.uuid_edit, 1, 1, 1, 3)
+        forward_button   = QPushButton('Forward', self)
+        left_button   = QPushButton('Left', self)
+        backward_button   = QPushButton('Backward', self)
+        right_button  = QPushButton('Right', self)
+        main_layout.addWidget(forward_button, 3, 2)
+        main_layout.addWidget(left_button, 4, 1)
+        main_layout.addWidget(backward_button, 4, 2)
+        main_layout.addWidget(right_button, 4, 3)
+        forward_button.clicked.connect(self.button_clicked)
+        left_button.clicked.connect(self.button_clicked)
+        backward_button.clicked.connect(self.button_clicked)
+        right_button.clicked.connect(self.button_clicked)
         self.show()
 
-    def sendCommand(self, command):
-        data = {'clientUID': self.uuidEdit.text(), 'command': command, 'args':[]}
+    def send_command(self, command):
+        data = {'clientUID': self.uuid_edit.text(), 'command': command, 'args':[]}
         headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
-        req = requests.post(self.ipEdit.text() + '/command', data=json.dumps(data), headers=headers)
+        requests.post(self.ip_edit.text() + '/command', data=json.dumps(data), headers=headers)
 
-    def buttonClicked(self):
+    def button_clicked(self):
         sender = self.sender()
-        self.sendCommand(sender.text())
+        self.send_command(sender.text())
 
-    def closeEvent(self, event):        
+    def close_event(self, event):
         reply = QMessageBox.question(self, 'Message',
             "Are you sure to quit?", QMessageBox.Yes | 
             QMessageBox.No, QMessageBox.No)
