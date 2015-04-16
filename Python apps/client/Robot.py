d1f08171e46b3735ce8ba64b3ea5a1a3ba97d8a9
KNV <bakaknv@gmail.com>
2015-04-16 20:16:51 +0300

    Собрал скетч для богомерзкой ардуины. Она умеет мигать светодиодиком в ответ на любую команду :))

--- Python apps/client/Robot.py
+++ Python apps/client/Robot.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python
 import threading
 from client.ClientAPI import ClientAPI
+import serial
 
 __author__ = 'baka'
+
+serial = serial.Serial('/dev/ttyACM0', 19200, timeout=1)
 
 class Robot(object):
     def __init__(self, url):
         self.clientApi = ClientAPI(url, 'CommandProxy')
         self.start_remote_command_listener()
+
 
     def start_remote_command_listener(self):
         recv_command_socket = self.clientApi.get_recv_sock_with_topic('command')
@@ -20,6 +24,13 @@ class Robot(object):
 
     def dispatch_command(self, json):
         print json
+        self.send_command(0, [4])
+
+    def send_command(self, command_code, command_args):
+        first = '\x15'
+        second = '\x90'
+        maxCount = bytes(bytearray([len(command_args) + 1]))
+        serial.write(first + second + maxCount + bytearray([command_code]) + bytes(bytearray(command_args)))
 
     def disconnect(self):
         self.clientApi.disconnect()
